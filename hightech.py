fb65f7df2c3eefd40bb43c059715908ad6a68150
Olga2001-Popova <82108597+Olga2001-Popova@users.noreply.github.com>
2022-01-19 21:51:26 +0300

    2site3

---
+++ hightech.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+def parserhightech(back_post_url):
+    URL = "https://hightech.fm/rubrics/tehnologii"
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+    post = soup.find("div", class_="news-feed__grid")
+    url = post.find("a", href=True)["href"].strip()
+
+    if url != back_post_url:
+        title = post.find("h2", class_="entry__title title title--size_sm truncate truncate--md").text.strip()
+        return f"{title}\n\n{URL + url}", url
+    else:
+        return None, url
--- main.py
+++ main.py
@@ -6,8 +6,7 @@ import time
 import meduza
 import nplus1
 import telebot
-import requests
-from bs4 import BeautifulSoup
+import hightech
 
 token = "5084782927:AAHXpO6WaA8uuXQCNl1Cwm9okbNjFfSTW1s"
 id_channel = "@news2k22botnews"
@@ -19,16 +18,21 @@ def commands(message):
         back_post_url = None
         back_post_id = None
         while True:
+            post_text_hightech = hightech.parserhightech(back_post_url)
+            back_post_url = post_text_hightech[1]
+            print(post_text_hightech[1])
+            print(post_text_hightech[0])
+
+            if post_text_hightech[0] != None:
+                bot.send_message(id_channel, post_text_hightech[0])
+                time.sleep(10)
+
             post_text_nplus1 = nplus1.parsernplus1(back_post_id)
             back_post_id = post_text_nplus1[1]
             if post_text_nplus1[0] != None:
                 bot.send_message(id_channel, post_text_nplus1[0])
-                time.sleep(60)
-            post_text_meduza= meduza.parsermeduza(back_post_url)
-            back_post_url = post_text_meduza[1]
-            if post_text_meduza[0] != None:
-                bot.send_message(id_channel, post_text_meduza[0])
-                time.sleep(60)
+                time.sleep(10)
+
     else:
         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши Старт")
 
