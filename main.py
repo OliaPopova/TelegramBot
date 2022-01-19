3a6faf1f61302aabebd20a8d6fd81881be16d70c
Olga2001-Popova <82108597+Olga2001-Popova@users.noreply.github.com>
2022-01-19 19:04:23 +0300

    2site19012022

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import time
-import mashable
+import meduza
 import nplus1
 import telebot
 import requests
@@ -16,15 +16,19 @@ bot = telebot.TeleBot(token)
 @bot.message_handler(content_types= ["text"])
 def commands(message):
     if message.text == "Старт":
+        back_post_url = None
         back_post_id = None
         while True:
-
             post_text_nplus1 = nplus1.parsernplus1(back_post_id)
             back_post_id = post_text_nplus1[1]
             if post_text_nplus1[0] != None:
                 bot.send_message(id_channel, post_text_nplus1[0])
-                time.sleep(20)
-
+                time.sleep(60)
+            post_text_meduza= meduza.parsermeduza(back_post_url)
+            back_post_url = post_text_meduza[1]
+            if post_text_meduza[0] != None:
+                bot.send_message(id_channel, post_text_meduza[0])
+                time.sleep(60)
     else:
         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши Старт")
 
---
+++ meduza.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+def parsermeduza(back_post_url):
+    URL = "https://meduza.io/"
+
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    post = soup.find("article", class_="RichBlock-root RichBlock-isMobile RichBlock-isRich")
+    url = post.find("a", class_="Link-root Link-isInBlockTitle", href=True)["href"].strip()
+
+    if url != back_post_url:
+        title = post.find("span", class_="BlockTitle-first").text.strip()
+        return f"{title}\n\n{URL + url}", url
+    else:
+        return None, url
