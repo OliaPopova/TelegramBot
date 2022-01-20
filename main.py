17d0f46b198c73a8934be25610726f6589418b10
Olga2001-Popova <82108597+Olga2001-Popova@users.noreply.github.com>
2022-01-20 13:33:23 +0300

    2site20012022

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import time
-import meduza
 import nplus1
 import telebot
 import hightech
@@ -12,28 +11,21 @@ token = "5084782927:AAHXpO6WaA8uuXQCNl1Cwm9okbNjFfSTW1s"
 id_channel = "@news2k22botnews"
 bot = telebot.TeleBot(token)
 
-@bot.message_handler(content_types= ["text"])
-def commands(message):
-    if message.text == "Старт":
-        back_post_url = None
-        back_post_id = None
-        while True:
-            post_text_hightech = hightech.parserhightech(back_post_url)
-            back_post_url = post_text_hightech[1]
-            print(post_text_hightech[1])
-            print(post_text_hightech[0])
 
-            if post_text_hightech[0] != None:
-                bot.send_message(id_channel, post_text_hightech[0])
-                time.sleep(60)
+@bot.message_handler(content_types=["text"])
+def commands():
+    back_post_url = None
+    back_post_id = None
+    while True:
+        post_text_hightech = hightech.parserhightech(back_post_url)
+        back_post_url = post_text_hightech[1]
 
-            post_text_nplus1 = nplus1.parsernplus1(back_post_id)
-            back_post_id = post_text_nplus1[1]
-            if post_text_nplus1[0] != None:
-                bot.send_message(id_channel, post_text_nplus1[0])
-                time.sleep(60)
+        if post_text_hightech[0] != None:
+            bot.send_message(id_channel, post_text_hightech[0])
+            time.sleep(10)
 
-    else:
-        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши Старт")
-
-bot.polling(none_stop=True, interval=0)
+        post_text_nplus1 = nplus1.parsernplus1(back_post_id)
+        back_post_id = post_text_nplus1[1]
+        if post_text_nplus1[0] != None:
+            bot.send_message(id_channel, post_text_nplus1[0])
+            time.sleep(10)
