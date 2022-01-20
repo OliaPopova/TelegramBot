28b83608c1c99d0fe9e380dec7ef3c88af5a3f16
Olga2001-Popova <82108597+Olga2001-Popova@users.noreply.github.com>
2022-01-20 21:31:48 +0300

    2sites

--- main.py
+++ main.py
@@ -18,19 +18,21 @@ def commands(message):
         back_post_url = None
         back_post_id = None
         while True:
-            post_text_hightech = hightech.parserhightech(back_post_url)
-            back_post_url = post_text_hightech[1]
-            if post_text_hightech[0] != None:
-                bot.send_message(id_channel, post_text_hightech[0])
-                time.sleep(60)
-
             post_text_nplus1 = nplus1.parsernplus1(back_post_id)
             back_post_id = post_text_nplus1[1]
             if post_text_nplus1[0] != None:
                 bot.send_message(id_channel, post_text_nplus1[0])
-                time.sleep(60)
+                time.sleep(900)
+
+            post_text_hightech = hightech.parserhightech(back_post_url)
+            back_post_url = post_text_hightech[1]
+            if post_text_hightech[0] != None:
+                bot.send_message(id_channel, post_text_hightech[0])
+                time.sleep(1800)
+
+
 
     else:
         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши Старт")
 
-bot.polling(none_stop=True, interval=0)
+bot.polling()
