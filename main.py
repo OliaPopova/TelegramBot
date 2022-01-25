fad409e34db4104e08ce652ba6bbe65e91a66912
Olga2001-Popova <82108597+Olga2001-Popova@users.noreply.github.com>
2022-01-25 20:28:38 +0300

    make it better

--- main.py
+++ main.py
@@ -15,7 +15,27 @@ bot = telebot.TeleBot(token)
 @bot.message_handler(content_types= ["text"])
 def commands(message):
     if message.text == "Старт":
-        bot.send_message(message.from_user.id, "Я запустился!")
+        back_post_url = None
+        back_post_url2 = None
+        back_post_id = None
+        while True:
+            post_text_nplus1 = nplus1.parsernplus1(back_post_id)
+            back_post_id = post_text_nplus1[1]
+            if post_text_nplus1[0] != None:
+                bot.send_message(id_channel, post_text_nplus1[0])
+                time.sleep(2)
+
+            post_text_hightech = hightech.parserhightech(back_post_url)
+            back_post_url = post_text_hightech[1]
+            if post_text_hightech[0] != None:
+                bot.send_message(id_channel, post_text_hightech[0])
+                time.sleep(2)
+
+            post_text_naked = naked_science.parsernaked_science(back_post_url2)
+            back_post_url2 = post_text_naked[1]
+            if post_text_naked[0] != None:
+                bot.send_message(id_channel, post_text_naked[0])
+                time.sleep(2)
     else:
         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши Старт")
 bot.polling()
