3c17e6ed3d16c007b271e553f4ffddcb3c53ff07
Olga2001-Popova <82108597+Olga2001-Popova@users.noreply.github.com>
2022-01-28 23:19:31 +0300

    bot

--- naked_science.py
+++ naked_science.py
@@ -5,8 +5,8 @@ from selenium import webdriver
 def parsernaked_science(back_post_url2):
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('headless')
-
-    driver = webdriver.Chrome(executable_path="C:/bin/chromedriver.exe", chrome_options=chrome_options)
+    chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_SHIM', None)
+    driver = webdriver.Chrome(executable_path="chromedriver", chrome_options=chrome_options)
 
     URL = "https://naked-science.ru/article/hi-tech"
     driver.get(URL)
